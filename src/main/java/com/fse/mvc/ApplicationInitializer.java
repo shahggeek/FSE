c9ed1372229b4e91972a6b9450c4e2ddee00d904
shaggeek <shah.gaurav1710@gmail.com>
2018-12-22 09:56:39 -0500

    tsest

--- src/main/java/com/fse/mvc/ApplicationInitializer.java
+++ src/main/java/com/fse/mvc/ApplicationInitializer.java
@@ -13,6 +13,7 @@ public class ApplicationInitializer implements WebApplicationInitializer {
     public void onStartup(ServletContext container) throws ServletException {
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.setServletContext(container);
+		
         ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
         servlet.setLoadOnStartup(1);
         servlet.addMapping("/");
